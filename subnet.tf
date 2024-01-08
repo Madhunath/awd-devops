8058ee7d414bbadbcb570dd9c627f0061cfb7120
madhunath <madhunath07@gmail.com>
2024-01-08 14:31:20 +0530

    commiting changes

--- subnet.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_subnet" "my_subnet" {
-  vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = var.subnet_cidr_block
-  availability_zone = var.availability_zone
-  tags = {
-    Name = "MySubnet"
-  }
-}
--- vpc.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_vpc" "my_vpc" {
-  cidr_block = var.vpc_cidr_block
-  tags = {
-    Name = "MyVPC"
-  }
-}
